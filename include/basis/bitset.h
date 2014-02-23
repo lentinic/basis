d4f970aaed426472f845f06384346e2ead10f792
Chris Lentini <lentinic@gmail.com>
2014-02-23 14:45:52 -0800

    Improved/fixed functionality for bitset class

--- include/basis/bitset.h
+++ include/basis/bitset.h
@@ -23,130 +23,174 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #pragma once
 
 #include <stdint.h>
+#include <string.h>
+#include <algorithm>
+#include <vector>
 
 namespace basis
 {
 	class bitset
 	{
 	public:
+		bitset()
+			:	m_bitCount(0)
+		{}
+
 		explicit bitset(size_t count)
 			:	m_bitCount(count),
-				m_intCount(count / INT_BITS)
+				m_bits(GetNumIntegersForBitcount(count))
+		{}
+
+		bitset(int * bits, size_t num_bits)
+			:	m_bitCount(num_bits), 
+				m_bits(bits, bits + GetNumIntegersForBitcount(num_bits))
+		{}
+
+		void set_bit(size_t index, bool b)
 		{
-			m_intCount = (m_intCount == 0) ? 1 : m_intCount;
-			m_bits = new int[m_intCount];
-			memset(m_bits, 0, sizeof(int) * m_intCount);
+			size_t int_index, bit_index;
+			bit_index_to_coordinates(index, &int_index, &bit_index);	
+
+			m_bits[int_index] = ((b ? 1 : 0) << bit_index) | (m_bits[int_index] & ~(1 << bit_index));
 		}
 
-		bitset(const bitset & other)
+		void set_or(size_t index, bool b)
 		{
-			m_bitCount = other.m_bitCount;
-			m_intCount = other.m_intCount;
-			m_bits = new int[m_intCount];
-			memcpy(m_bits, other.m_bits, sizeof(int) * m_intCount);
+			size_t int_index, bit_index;
+			bit_index_to_coordinates(index, &int_index, &bit_index);
+
+			m_bits[int_index] = ((b ? 1 : 0) << bit_index) | m_bits[int_index];
 		}
 
-		bitset & operator = (const bitset & other)
+		void set_xor(size_t index, bool b)
 		{
-			m_bitCount = other.m_bitCount;
-			m_intCount = other.m_intCount;
-			delete [] m_bits;
-			m_bits = new int[m_intCount];
-			memcpy(m_bits, other.m_bits, sizeof(int) * m_intCount);
-			return *this;
+			size_t int_index, bit_index;
+			bit_index_to_coordinates(index, &int_index, &bit_index);
+
+			m_bits[int_index] = ((b ? 1 : 0) << bit_index) ^ m_bits[int_index];
 		}
 
-		bitset(bitset && other)
+		void set_and(size_t index, bool b)
 		{
-			m_bitCount = other.m_bitCount;
-			m_intCount = other.m_intCount;
-			m_bits = other.m_bits;
-			other.m_bits = nullptr;
+			size_t int_index, bit_index;
+			bit_index_to_coordinates(index, &int_index, &bit_index);
 
-		}
-
-		bitset & operator = (bitset && other)
-		{
-			m_bitCount = other.m_bitCount;
-			m_intCount = other.m_intCount;
-			delete [] m_bits;
-			m_bits = other.m_bits;
-			other.m_bits = nullptr;			
-			return *this;	
-		}
-
-		int set_or(size_t index, int b)
-		{
-			size_t pos;
-			get_offsets(index, &pos, &index);
-
-			int r = m_bits[pos] |= (b << index);
-			return (r >> index) & 1;
-		}
-
-		int set_xor(size_t index, int b)
-		{
-			size_t pos;
-			get_offsets(index, &pos, &index);
-
-			int r = m_bits[pos] ^= (b << index);
-			return (r >> index) & 1;
-		}
-
-		int set_and(size_t index, int b)
-		{
-			size_t pos;
-			get_offsets(index, &pos, &index);
-
-			int r = m_bits[pos] &= (b << index);
-			return (r >> index) & 1;
+			m_bits[int_index] = ((~0) ^ ((b ? 0 : 1) << bit_index)) & m_bits[int_index];
 		}
 
 		void set_all_one()
 		{
-			memset(m_bits, 0xff, m_intCount * sizeof(int));
+			memset(&m_bits[0], 0xff, m_bits.size() * sizeof(int));
 		}
 
 		void set_all_zero()
 		{
-			memset(m_bits, 0xff, m_intCount * sizeof(int));
+			memset(&m_bits[0], 0xff, m_bits.size() * sizeof(int));
 		}
 
 		bool test_all_one()
+		{ 
+			for (size_t i=0, end=m_bits.size(); i<end; i++)
+			{
+				int v = m_bits[i];
+				if (v != (~0))
+				{
+					if (i == (end - 1))
+					{
+						size_t leftover = (end * INT_BITS) - m_bitCount;
+						int mask = (~0) >> leftover;
+						return (v & mask) == mask;
+					}
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		bitset operator | (const bitset & b) const
 		{
-			int * cur = m_bits;
-			int * end = cur + m_intCount;
-			int v = 0;
-			while (cur != end)
-			{
-				v = *cur;
-				cur++;
+			size_t max_bits = std::max(m_bitCount, b.m_bitCount);
+			size_t min_bits = std::min(m_bitCount, b.m_bitCount);
+			size_t min_ints = GetNumIntegersForBitcount(min_bits);
+			size_t max_ints = GetNumIntegersForBitcount(max_bits);
 
-				if (v != 0xffffffff)
-					break;
+			int * bits = new int[max_ints];
+			for (size_t i=0; i<min_ints; i++)
+			{
+				bits[i] = m_bits[i] | b.m_bits[i];
 			}
 
-			if (cur == end)
+			for (size_t i=min_ints; i<max_ints; i++)
 			{
-				size_t leftover = (m_intCount * INT_BITS) - m_bitCount;
-				int mask = 0xffffffff >> leftover;
-				return (v & mask) == mask;
+				bits[i] = (m_bitCount > b.m_bitCount) ? m_bits[i] : b.m_bits[i];
 			}
 
-			return false;
+			return bitset(bits, max_bits);
+		}
+
+		bitset operator & (const bitset & b) const
+		{
+			size_t max_bits = std::max(m_bitCount, b.m_bitCount);
+			size_t min_bits = std::min(m_bitCount, b.m_bitCount);
+			size_t min_ints = GetNumIntegersForBitcount(min_bits);
+			size_t max_ints = GetNumIntegersForBitcount(max_bits);
+
+			int * bits = new int[max_ints];
+			memset(bits, 0, sizeof(int) * max_ints);
+			for (size_t i=0; i<min_ints; i++)
+			{
+				bits[i] = m_bits[i] & b.m_bits[i];
+			}
+
+			return bitset(bits, max_bits);
+		}
+
+		bitset operator ^ (const bitset & b) const
+		{
+			size_t max_bits = std::max(m_bitCount, b.m_bitCount);
+			size_t min_bits = std::min(m_bitCount, b.m_bitCount);
+			size_t min_ints = GetNumIntegersForBitcount(min_bits);
+			size_t max_ints = GetNumIntegersForBitcount(max_bits);
+
+			int * bits = new int[max_ints];
+			for (size_t i=0; i<min_ints; i++)
+			{
+				bits[i] = m_bits[i] ^ b.m_bits[i];
+			}
+
+			for (size_t i=min_ints; i<max_ints; i++)
+			{
+				bits[i] = (m_bitCount > b.m_bitCount) ? m_bits[i] : b.m_bits[i];
+			}
+
+			return bitset(bits, max_bits);
+		}
+
+		int operator [] (size_t index) const
+		{
+			size_t int_index, bit_index;
+			bit_index_to_coordinates(index, &int_index, &bit_index);
+			return (m_bits[int_index] >> bit_index) & 1;
 		}
 
 	private:
-		size_t		m_bitCount;
-		size_t		m_intCount;
-		int * 		m_bits;
+		size_t				m_bitCount;
+		std::vector<int>	m_bits;
 
-		void get_offsets(size_t index, size_t * a, size_t * b)
+		void bit_index_to_coordinates(size_t bit, size_t * int_index, size_t * bit_index) const
 		{
-			size_t pos = index / INT_BITS;
-			BASIS_ASSERT(pos < m_intCount);
-			*a = pos;
-			*b = index - (pos * INT_BITS);
+			size_t i = bit / INT_BITS;
+
+			BASIS_ASSERT(i < m_bits.size());
+
+			*int_index = i;
+			*bit_index = bit - (i * INT_BITS);
+		}
+
+		static size_t GetNumIntegersForBitcount(size_t num_bits)
+		{
+			return (num_bits + (INT_BITS - 1)) / INT_BITS;
 		}
 
 		static const size_t INT_BITS = (sizeof(int) * 8); 
