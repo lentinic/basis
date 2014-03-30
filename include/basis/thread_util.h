4ce403f3dc4d0903a1f9b2223d7ba3771d085443
Chris Lentini <lentinic@gmail.com>
2014-03-30 15:39:10 -0700

    renamed to cpu_yield

--- include/basis/thread_util.h
+++ include/basis/thread_util.h
@@ -24,7 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 namespace basis
 {
-	inline void cpu_pause()
+	inline void cpu_yield()
 	{
 #if defined(X86)
 		_mm_pause();
