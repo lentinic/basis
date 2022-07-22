d242066ddf068bac1c8ed81ed7c2664df98ad8e7
Chris Lentini <lentinic@gmail.com>
2022-07-21 21:44:25 -0700

    remove debug.h, change debugbreak to a macro defined in assert.h

--- include/basis/assert.h
+++ include/basis/assert.h
@@ -7,27 +7,36 @@ This source code is licensed under the MIT license (found in the LICENSE file in
 
 #pragma once
 
-#include "debug.h"
 #include "macros.h"
+
+#if defined(_MSC_VER)
+#define BASIS_BREAKPOINT() BASIS_MACRO_BLOCK(__debugbreak();)
+#elif defined(__GNUC__) || defined(__clang__)
+#define BASIS_BREAKPOINT() BASIS_MACRO_BLOCK(__builtin_trap();)
+#elif defined(__x86_64__) || defined(__i386__)
+#define BASIS_BREAKPOINT() BASIS_MACRO_BLOCK(__asm__ ("int $3");)
+#else
+#define BASIS_BREAKPOINT() BASIS_MACRO_BLOCK(*((int *) 0) = 3; })
+#endif
 
 #if defined(_DEBUG) || defined(BASIS_ASSERT_ENABLED)
 
 #define BASIS_ASSERT_BASIC(cnd) \
-    BASIS_MACRO_BLOCK( if (!(cnd)) { basis::DebugBreak(); } )
+    BASIS_MACRO_BLOCK( if (!(cnd)) { BASIS_BREAKPOINT(); } )
 
 #define BASIS_ASSERT_SIGNAL(cnd, signal) \
-    BASIS_MACRO_BLOCK( if (!(cnd) && (signal)()) { basis::DebugBreak(); } )
+    BASIS_MACRO_BLOCK( if (!(cnd) && (signal)()) { BASIS_BREAKPOINT(); } )
 
-#define BASIS_ASSERT_FAILED BASIS_MACRO_BLOCK(basis::DebugBreak();)
+#define BASIS_ASSERT_FAILED BASIS_BREAKPOINT()
 
 #else
 
 #define BASIS_ASSERT_BASIC(cnd) BASIS_UNUSED(cnd)
 #define BASIS_ASSERT_SIGNAL(cnd, signal) BASIS_UNUSED(cnd); BASIS_UNUSED(signal)
 #define BASIS_ASSERT_FAILED
-
+ 
 #endif
-
+ 
 #define BASIS_ASSERT_SELECTOR(tuple) BASIS_ASSERT_SELECTOR_IMPL tuple
 #define BASIS_ASSERT_SELECTOR_IMPL(_1,_2,N,...) N
 
